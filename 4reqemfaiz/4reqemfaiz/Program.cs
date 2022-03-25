1d4549e281a86f68e9021bfdba525844f8ad9012
ashraf032 <101041982+ashraf032@users.noreply.github.com>
2022-03-25 19:15:50 +0400

    result commit

---
+++ 4reqemfaiz/4reqemfaiz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4reqemfaiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir 4 reqemli eded verin");
+            string num1 = Console.ReadLine();
+
+            int a = int.Parse(num1);
+
+            double b = a / 100 * 20;
+
+
+            double c = b / 100 * 10;
+
+            if (a >= 1000 && a < 10000)
+            {
+                Console.WriteLine($"{c * c}");
+            }
+            else
+            {
+                Console.WriteLine("Bu hall nezere alinmayib");
+            }
+        }
+    }
+}
